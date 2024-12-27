b9b4c27aaaeda9328191512d1f0835216c8255f1
Alex Leventis <alex.leventis@bit.ly>
2024-12-27 11:00:35 -0800

    init: client/server local area network http remote

---
+++ client/app/api.ts
@@ -0,0 +1,69 @@
+// set client <-> server port
+const port = 5001;
+
+// valid endpoints configured in server
+export type ValidEndpoint = "discover_ping" | "os_action"
+
+// valid http methods for request
+export type HttpMethod = "GET" | "POST" // ...etc
+
+// sendRequest is a helper function that sends a general http request and returns the full response
+export const sendRequest = async (method: HttpMethod, endpoint: ValidEndpoint, ip: string, signal?: AbortSignal, queryParams?: URLSearchParams): Promise<Response> => {
+  let url = `http://${ip}:${port}/${endpoint}`;
+
+  const queryString = queryParams?.toString();
+  if (queryString) {
+    url += `?${queryString}`;
+  }
+
+  const response = await fetch(url, { 
+    method: method,
+    signal: signal,
+  });
+  return response
+}
+
+export const scanNetwork = async (setServerIP: React.Dispatch<React.SetStateAction<string>>) => {
+  const response = await fetch('/api/scan');
+  const data = await response.json();
+  if (data.devices.length > 0) {
+    setServerIP(data.devices[0].address)
+    return
+  }
+  setServerIP("no server found")
+}
+
+// triggerKeyPress sends the keypress action to the /key endpoint in server.py
+export const triggerKeyPress = async (serverIP: string, key_action: string) => {
+  try {
+    const queryParams = new URLSearchParams();
+    queryParams.append("action", key_action);
+
+    const response = await sendRequest("GET", "os_action", serverIP, undefined, queryParams);
+    if (!response.ok) {
+      alert(`Failed to send request: ${response.status}`);
+    }
+    return response
+  } catch (error) {
+    alert(`Failed to send request: ${(error as Error).message}`);
+  }
+};
+
+// adjustVolume triggers key press and set's the vollume state to value received from server
+export const adjustVolume = async(serverIP: string, key_action: string, setVolume: React.Dispatch<React.SetStateAction<string>>) => {
+  const res = await triggerKeyPress(serverIP, key_action)
+  if (res && res.ok) {
+    const data = await res.json();
+    if (data.volume !== "") {
+      setVolume(data.volume);
+    }
+  } else {
+    console.log("failed to adjust volume");
+  }
+}
+
+// reScan scans the network again for /discovery_ping response in case of new network location
+export const reScan = (i: number, incrRescan: React.Dispatch<React.SetStateAction<number>>, setServerIP: React.Dispatch<React.SetStateAction<string>>) => {
+  setServerIP("...searching")
+  incrRescan(i + 1)
+}
---
+++ server/handlers.py
@@ -0,0 +1,56 @@
+import subprocess
+import logging
+
+logger = logging.getLogger("flask.app")
+
+# valid request actions
+KEY_MAPPING = {
+    "play_pause": 100,
+    "previous_track": 98,
+    "next_track": 101,
+    "left_arrow": 123,
+    "right_arrow": 124,
+    "brightness_up": 144,
+    "brightness_down": 145,
+}
+
+OS_KEY_MAPPING = {
+    "volume_up": lambda: adjust_volume("up"),
+    "volume_down": lambda: adjust_volume("down"),
+    "current": lambda: adjust_volume("current")
+}
+
+# handlers
+
+def handleKeyPress(action):
+    if action not in KEY_MAPPING:
+        return {'error': 'Invalid key action'}
+    
+    key_code = KEY_MAPPING[action]
+    subprocess.run(['osascript', '-e', f'tell application "System Events" to key code {key_code}'])
+    return {'status': f'{action}'}
+
+def handleCustomOSAction(action):
+    if action not in OS_KEY_MAPPING:
+        return {'error': 'Invalid OS action'}
+    return OS_KEY_MAPPING[action]()
+
+# helper for volume adjustment
+def adjust_volume(command):
+    current_volume_script = 'output volume of (get volume settings)'
+    result = subprocess.run(['osascript', '-e', current_volume_script], capture_output=True, text=True)
+    current_volume = int(result.stdout.strip())
+
+    if command == 'current':
+        new_volume = current_volume
+    elif command == 'up':
+        new_volume = min(current_volume + 10, 100)
+    elif command == 'down':
+        new_volume = max(current_volume - 10, 0)
+    else:
+        return {'error': 'invalid command'}
+
+    set_volume_script = f'set volume output volume {new_volume}'
+    subprocess.run(['osascript', '-e', set_volume_script])
+
+    return {'status': f'{command}', 'volume': f'{new_volume}'}
