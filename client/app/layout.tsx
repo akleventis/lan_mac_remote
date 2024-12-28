88f81b2a68f3af7090d3de1ffcb63ed353b9e36f
Alex Leventis <alex.leventis@bit.ly>
2024-12-28 12:00:56 -0800

    icons/styling

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <HomeScreen/>
-        {children}
+        {/* {children} */}
         </body>
     </html>
   )
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import React, { useState, useEffect } from 'react';
+import Image from 'next/image';
 import { scanNetwork, triggerKeyPress, adjustVolume, reScan } from "./api";
 
 // manually add ip in the overrideIP variable below to skip network scan
@@ -27,34 +28,53 @@ export default function HomeScreen() {
   return (
     <div style={styles.container}>
       <div>ip: {serverIP}</div>
-      <button style={styles.button} onClick={() => triggerKeyPress(serverIP, "play_pause")}>
-        play/pause
-      </button>
-      <button style={styles.button} onClick={() => triggerKeyPress(serverIP, "previous_track")}>
-        previous track
-      </button>
-      <button style={styles.button} onClick={() => triggerKeyPress(serverIP, "next_track")}>
-        next track
-      </button>
-      <button style={styles.button} onClick={() => triggerKeyPress(serverIP, "left_arrow")}>
-        left arrow key
-      </button>
-      <button style={styles.button} onClick={() => triggerKeyPress(serverIP, "right_arrow")}>
-        right arrow key
-      </button>
-      <button style={styles.button} onClick={() => triggerKeyPress(serverIP, "brightness_up")}>
-        brightness up
-      </button>
-      <button style={styles.button} onClick={() => triggerKeyPress(serverIP, "brightness_down")}>
-        brightness down
-      </button>
-      <button style={styles.button} onClick={() => adjustVolume(serverIP, "volume_up", setVolume)}>
-        volume up
-      </button>
-      <button style={styles.button} onClick={() => adjustVolume(serverIP, "volume_down", setVolume)}>
-        volume down
-      </button>
-      <button style={styles.button} onClick={() => reScan(i, incrRescan, setServerIP)}>rescan</button>
+
+      <div style={styles.row}>
+        <button style={styles.item} onClick={() => triggerKeyPress(serverIP, "previous_track")}>
+          <Image width="25" height="25" alt="previous track" src={`/images/track-prev.png`} />
+        </button>
+        <button style={styles.item} onClick={() => triggerKeyPress(serverIP, "next_track")}>
+          <Image width="25" height="25" alt="next track" src={`/images/track-next.png`} />
+        </button>
+      </div>
+      
+      <div style={styles.row}>
+        <button style={styles.item} onClick={() => triggerKeyPress(serverIP, "left_arrow")}>
+          <Image width="25" height="25" alt="left arrow key" src={`/images/arrow-left.png`} />
+        </button>
+        <button style={styles.item} onClick={() => triggerKeyPress(serverIP, "play_pause")}>
+          <Image width="25" height="25" alt="play/pause" src={`/images/play.png`} />
+        </button>
+        <button style={styles.item} onClick={() => triggerKeyPress(serverIP, "right_arrow")}>
+          <Image width="25" height="25" alt="right arrow key" src={`/images/arrow-right.png`} />
+        </button>
+      </div>
+
+      <div style={styles.buttonRow}>
+        <div style={styles.column}>
+          <button style={styles.item} onClick={() => triggerKeyPress(serverIP, "brightness_up")}>
+            <Image width="25" height="25" alt="brightness up" src={`/images/brightness-up.png`} />
+          </button>
+          <button style={styles.item} onClick={() => triggerKeyPress(serverIP, "brightness_down")}>
+            <Image width="25" height="25" alt="brightness down" src={`/images/brightness-down.png`} />
+          </button>
+        </div>
+
+        <button style={styles.item} onClick={() => reScan(i, incrRescan, setServerIP)}>
+          <Image width="25" height="25" alt="rescan" src={`/images/rescan.png`} />
+        </button>
+
+        <div style={styles.column}>
+          <button style={styles.item} onClick={() => adjustVolume(serverIP, "volume_up", setVolume)}>
+            <Image width="25" height="25" alt="volume up" src={`/images/volume-up.png`} />
+          </button>
+          <button style={styles.item} onClick={() => adjustVolume(serverIP, "volume_down", setVolume)}>
+            <Image width="25" height="25" alt="volume down" src={`/images/volume-down.png`} />
+          </button>
+        </div>
+
+      </div>
+
       <div>volume: {volume}</div>
     </div>
   );
@@ -69,7 +89,30 @@ const styles = {
     justifyContent: "center",
     height: "100vh",
   },
-  button: {
-    margin: "5px 0",
-  }
+  item: {
+    margin: 10,
+    width: 50,
+    height: 50,
+    padding: 5,
+    borderRadius: 10,
+    borderColor: '#d26ffc',
+  },
+  column: {
+    display: 'flex',
+    flex: 1,
+    flexDirection: 'column' as 'column',
+    justifyContent: 'center',
+  },
+  row: {
+    display: 'flex',
+    flexDirection: 'row' as 'row',
+  },
+  buttonRow: {
+    display: 'flex',
+    flexDirection: 'row' as 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginLeft: 80,
+    marginRight: 80,
+  },
 };
